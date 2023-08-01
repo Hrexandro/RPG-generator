4b6da0d06ea844e1974f33e4e00fe520862f851d
Domowe <marcin_nowak_hrx@o2.pl>
2023-08-01 22:53:40 +0200

    try debugging delayed class pick again

--- script.js
+++ script.js
@@ -1810,6 +1810,7 @@ function pickFromList(pickedList) {
 
 generateButton.addEventListener('click', () => {
   updatePick()
+  updatePickedClass()
   const result = []
   const pickedCategory = eval(category)
   removeAllChildren(nameDisplay)
